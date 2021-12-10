4c2d344a86d90de7328b37c34e8c9316b679c29a
Tuanndq <quangtuanxyz@gmail.com>
2021-12-10 09:20:50 +0700

    'update'

--- app.py
+++ app.py
@@ -25,58 +25,18 @@ def map_func():
     r = requests.get(url=URL, params=PARAMS)
     data = r.json()
 
-
-    hospitalOne = data['items'][0]['title']
-    hospitalOne_address = data['items'][0]['address']['label']
-    hospitalOne_latitude = data['items'][0]['position']['lat']
-    hospitalOne_longitude = data['items'][0]['position']['lng']
-
-
-    hospitalTwo = data['items'][1]['title']
-    hospitalTwo_address = data['items'][1]['address']['label']
-    hospitalTwo_latitude = data['items'][1]['position']['lat']
-    hospitalTwo_longitude = data['items'][1]['position']['lng']
-
-    hospitalThree = data['items'][2]['title']
-    hospitalThree_address = data['items'][2]['address']['label']
-    hospitalThree_latitude = data['items'][2]['position']['lat']
-    hospitalThree_longitude = data['items'][2]['position']['lng']
-
-
-    hospitalFour = data['items'][3]['title']
-    hospitalFour_address = data['items'][3]['address']['label']
-    hospitalFour_latitude = data['items'][3]['position']['lat']
-    hospitalFour_longitude = data['items'][3]['position']['lng']
-
-    hospitalFive = data['items'][4]['title']
-    hospitalFive_address = data['items'][4]['address']['label']
-    hospitalFive_latitude = data['items'][4]['position']['lat']
-    hospitalFive_longitude = data['items'][4]['position']['lng']
-    return render_template('map.html',
-                           latitude=latitude,
-                           longitude=longitude,
-                           apikey=api_key,
-                           oneName=hospitalOne,
-                           OneAddress=hospitalOne_address,
-                           oneLatitude=hospitalOne_latitude,
-                           oneLongitude=hospitalOne_longitude,
-                           twoName=hospitalTwo,
-                           twoAddress=hospitalTwo_address,
-                           twoLatitude=hospitalTwo_latitude,
-                           twoLongitude=hospitalTwo_longitude,
-                           threeName=hospitalThree,
-                           threeAddress=hospitalThree_address,
-                           threeLatitude=hospitalThree_latitude,
-                           threeLongitude=hospitalThree_longitude,
-                           fourName=hospitalFour,
-                           fourAddress=hospitalFour_address,
-                           fourLatitude=hospitalFour_latitude,
-                           fourLongitude=hospitalFour_longitude,
-                           fiveName=hospitalFive,
-                           fiveAddress=hospitalFive_address,
-                           fiveLatitude=hospitalFive_latitude,
-                           fiveLongitude=hospitalFive_longitude
-                           )
+    print(data)
+    
+    result = []
+    for i, ele in enumerate(data['items']):
+        address = {
+            "title": ele['title'],
+            "address": ele['address']['label'],
+            "latitude": ele['position']['lat'],
+            "longitude": ele['position']['lng'],
+        }
+        result.append(address)
+    return jsonify(result)
 
 
 if __name__ == '__main__':
